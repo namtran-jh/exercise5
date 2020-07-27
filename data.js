88b64d83d0bd1d73806ca02d2753c4c55f8ec098
thnammm <nam.tran@journeyh.io>
2020-07-27 16:21:43 +0700

    done carousel list picture

---
+++ data.js
@@ -0,0 +1,31 @@
+var data = [{
+    id: 1,
+    linkImg: "./images/pic1.png",
+    caption: "HASHIRAMA",
+    content: "First Hokage of Leaf Village"
+}, {
+    id: 2,
+    linkImg: "./images/pic2.png",
+    caption: "TOBIRAMA",
+    content: "Second Hokage of Leaf Village"
+}, {
+    id: 3,
+    linkImg: "./images/pic3.png",
+    caption: "SARUTOBI",
+    content: "Third Hokage of Leaf Village"
+}, {
+    id: 4,
+    linkImg: "./images/pic4.png",
+    caption: "MINATO",
+    content: "Fourth Hokage of Leaf Village"
+}, {
+    id: 5,
+    linkImg: "./images/pic5.png",
+    caption: "TSUNADE",
+    content: "Fifth Hokage of Leaf Village"
+}, {
+    id: 6,
+    linkImg: "./images/pic6.png",
+    caption: "KAKASHI",
+    content: "Sixth Hokage of Leaf Village"
+}];
