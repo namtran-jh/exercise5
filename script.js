cda3de71f2d4c7bc936bb8a352f82d42db1cbbd3
thnamm <nam.tran@journeyh.io>
2020-08-19 17:28:15 +0700

    Refactor script.js

--- script.js
+++ script.js
@@ -1,96 +1,90 @@
-const mainImage = {};
+const showingPicture = {};
+
+const MANUAL = 0;
+const AUTO = 1;
 const modeSwitch = {
-    type: 0
+    type: MANUAL
 };
 
-// Initial list pictures
-let listPicHTML = "";
+let mode; // variable hold interval method
+
+
+let listPictureHTML = "";
 data.forEach(val => {
-    listPicHTML += '<div class="carouselPic" onclick="changePic(' + val.id + ')">';
-    listPicHTML += '<img id="pic' + val.id + '" src="' + val.linkImg + '" alt="pic' + val.id + '" />';
-    listPicHTML += '</div>';
+    listPictureHTML += '<div class="carouselPicture" onclick="changePicture(' + val.id + ')">';
+    listPictureHTML += '<img id="pic' + val.id + '" src="' + val.linkPicture + '" alt="pic' + val.id + '" />';
+    listPictureHTML += '</div>';
 })
-document.getElementById("listPics").innerHTML = listPicHTML;
+document.getElementById("listPictures").innerHTML = listPictureHTML;
 
-// Change image
-function changePic(id) {
-    if (id < 1) id = 6;
-    if (id > 6) id = 1;
 
-    const image = data.filter(val => val.id === id);
+function changePicture(pictureId) {
+    if (pictureId < 1) pictureId = 6; // when we move backward over the first picture, set showing state to the last picture
+    if (pictureId > 6) pictureId = 1; // vice versa
 
-    document.getElementById("mainPic").src = image[0].linkImg;
-    document.getElementById("mainPic").alt = image[0].caption.toLowerCase();
-    document.getElementById("caption").innerHTML = image[0].caption;
-    document.getElementById("content").innerHTML = image[0].content;
+    const picture = data.filter(val => val.id === pictureId);
 
-    setActiveForMainImg(image[0]);
-    restartMainImg(image[0]);
+    document.getElementById("showingPicture").src = picture[0].linkPicture;
+    document.getElementById("showingPicture").alt = picture[0].caption.toLowerCase();
+    document.getElementById("pictureCaption").innerHTML = picture[0].caption;
+    document.getElementById("pictureContent").innerHTML = picture[0].content;
+
+    setActiveForShowingPicture(picture[0]);
+    restartShowingPicture(picture[0]);
 
     const element = document.getElementById("main");
-    element.classList.remove("animationMainPic");
+    element.classList.remove("animationMainPicture");
     void element.offsetWidth;
-    element.classList.add("animationMainPic");
+    element.classList.add("animationMainPicture");
 }
 
-// Restart main image
-function restartMainImg(image) {
-    mainImage.id = image.id;
-    mainImage.linkImg = image.linkImg;
-    mainImage.caption = image.caption;
-    mainImage.content = image.content;
+// Restart showingPicture global variable
+function restartShowingPicture(picture) {
+    showingPicture.id = picture.id;
+    showingPicture.linkPicture = picture.linkImg;
+    showingPicture.caption = picture.caption;
+    showingPicture.content = picture.content;
 }
 
 // Go backward and forward
-function move(direction) {
-    changePic(mainImage.id + direction);
-    if (modeSwitch.type === 1) modeAuto();
+function movePicture(direction) {
+    changePicture(showingPicture.id + direction);
+    if (modeSwitch.type === AUTO) setModeAuto();
 }
 
-// Set active class
-function setActiveForMainImg(image) {
-    let oldImg = document.getElementById("pic" + mainImage.id);
-    oldImg.classList.remove("active");
 
-    let newImg = document.getElementById("pic" + image.id);
-    newImg.classList.add("active");
+function setActiveForShowingPicture(picture) {
+    let oldPicture = document.getElementById("pic" + showingPicture.id);
+    oldPicture.classList.remove("active");
+
+    let newPicture = document.getElementById("pic" + picture.id);
+    newPicture.classList.add("active");
 }
 
-restartMainImg(data[0]);
-changePic(data[0].id);
-
-// Switch modes
-let mode;
-
-function modeManual() {
-    modeSwitch.type = 0;
+function setModeManual() {
+    modeSwitch.type = MANUAL;
     clearInterval(mode);
 }
 
-function modeAuto() {
+function setModeAuto() {
     clearInterval(mode);
-    modeSwitch.type = 1;
-    // let tmp = mainImage.id
+    modeSwitch.type = AUTO;
     mode = setInterval(function() {
-        move(1);
-        //     console.log(tmp, mainImage.id)
-        //     if (tmp !== mainImage.id) {
-        //         clearInterval(mode);
-        //         modeAuto();
-        //     } else {
-        //         move(1);
-        //     }
+        movePicture(1);
     }, 3000)
 }
 
-// Set key down
+// Set key down for moving backward, forward
 document.addEventListener("keydown", setKeyDown, false);
 
 function setKeyDown(event) {
     if (event.keyCode === 37) {
-        move(-1);
+        movePicture(-1);
     }
     if (event.keyCode === 39) {
-        move(1);
+        movePicture(1);
     }
 }
+
+restartShowingPicture(data[0]);
+changePicture(data[0].id);
